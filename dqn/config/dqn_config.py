3581ddc16f89f47ffeec6e724a4ad89a47e854be
romainducrocq <romain.g.ducrocq@gmail.com>
2021-07-09 15:40:38 +0200

    convnet 1st architecture

--- dqn/config/dqn_config.py
+++ dqn/config/dqn_config.py
@@ -1,34 +1,37 @@
+import torch as T
 import torch.nn as nn
 import torch.optim as optim
 
+CONFIG = "1tls_3x3"
+
 # """CHANGE HYPER PARAMETERS HERE""" ###################################################################################
 HYPER_PARAMS = {
-    'gpu': '0',                          # GPU #
-    'n_env': 4,                          # Multi-processing environments
-    'lr': 1e-04,                         # Learning rate
-    'gamma': 0.99,                       # Discount factor
-    'eps_start': 1.,                     # Epsilon start
-    'eps_min': 0.01,                     # Epsilon min
-    'eps_dec': 5e6,                      # Epsilon decay
-    'eps_dec_exp': True,                 # Epsilon exponential decay
-    'bs': 32,                            # Batch size
-    'min_mem': 1000000,                  # Replay memory buffer min size
-    'max_mem': 1000000,                  # Replay memory buffer max size
-    'target_update_freq': 30000,         # Target network update frequency
-    'target_soft_update': True,          # Target network soft update
-    'target_soft_update_tau': 1e-03,     # Target network soft update tau rate
-    'save_freq': 10000,                  # Save frequency
-    'log_freq': 1000,                    # Log frequency
-    'save_dir': './save/',               # Save directory
-    'log_dir': './logs/train/',          # Log directory
-    'load': True,                        # Load model
-    'repeat': 5,                         # Repeat action
-    'max_episode_steps': 5000,           # Time limit episode steps
-    'max_total_steps': 0,                # Max total training steps if > 0, else inf training
-    'algo': 'PerDuelingDoubleDQNAgent'   # DQNAgent
-                                         # DoubleDQNAgent
-                                         # DuelingDoubleDQNAgent
-                                         # PerDuelingDoubleDQNAgent
+    'gpu': '0',                                 # GPU #
+    'n_env': 0,                                 # Multi-processing environments
+    'lr': 1e-04,                                # Learning rate
+    'gamma': 0.99,                              # Discount factor
+    'eps_start': 1.,                            # Epsilon start
+    'eps_min': 0.1,                             # Epsilon min
+    'eps_dec': 1e5,                             # Epsilon decay
+    'eps_dec_exp': True,                        # Epsilon exponential decay
+    'bs': 32,                                   # Batch size
+    'min_mem': 100000,                          # Replay memory buffer min size
+    'max_mem': 1000000,                         # Replay memory buffer max size
+    'target_update_freq': 30000,                # Target network update frequency
+    'target_soft_update': True,                 # Target network soft update
+    'target_soft_update_tau': 1e-03,            # Target network soft update tau rate
+    'save_freq': 10000,                         # Save frequency
+    'log_freq': 1000,                           # Log frequency
+    'save_dir': './save/' + CONFIG + "/",       # Save directory
+    'log_dir': './logs/train/' + CONFIG + "/",  # Log directory
+    'load': True,                               # Load model
+    'repeat': 0,                                # Repeat action
+    'max_episode_steps': 0,                     # Time limit episode steps
+    'max_total_steps': 0,                       # Max total training steps if > 0, else inf training
+    'algo': 'DuelingDoubleDQNAgent'             # DQNAgent
+                                                # DoubleDQNAgent
+                                                # DuelingDoubleDQNAgent
+                                                # PerDuelingDoubleDQNAgent
 }
 
 ########################################################################################################################
@@ -37,14 +40,31 @@ HYPER_PARAMS = {
 # """CHANGE NETWORK CONFIG HERE""" #####################################################################################
 def network_config(input_dim):
     # """CHANGE NETWORK HERE""" ########################################################################################
-    hidden_dims = (32, 32)
+    cnn_dims = (
+        (16, 4, 2),
+        (32, 2, 1)
+    )
+
+    fc_dims = (128, 64)
 
     activation = nn.ELU()
 
-    net = nn.Sequential(
-        nn.Linear(input_dim, hidden_dims[0]),
+    cnn = nn.Sequential(
+        nn.Conv2d(input_dim.shape[0], cnn_dims[0][0], kernel_size=cnn_dims[0][1], stride=cnn_dims[0][2]),
         activation,
-        nn.Linear(hidden_dims[0], hidden_dims[1]),
+        nn.Conv2d(cnn_dims[0][0], cnn_dims[1][0], kernel_size=cnn_dims[1][1], stride=cnn_dims[1][2]),
+        activation,
+        nn.Flatten()
+    )
+
+    with T.no_grad():
+        n_flatten = cnn(T.as_tensor(input_dim.sample()[None]).float()).shape[1]
+
+    net = nn.Sequential(
+        cnn,
+        nn.Linear(n_flatten, fc_dims[0]),
+        activation,
+        nn.Linear(fc_dims[0], fc_dims[1]),
         activation
     )
     ####################################################################################################################
@@ -58,7 +78,7 @@ def network_config(input_dim):
     ####################################################################################################################
 
     # """CHANGE FC DUELING LAYER OUTPUT DIM HERE""" ####################################################################
-    fc_out_dim = hidden_dims[-1]
+    fc_out_dim = fc_dims[-1]
     ####################################################################################################################
 
     return net, optim_func, loss_func, fc_out_dim
