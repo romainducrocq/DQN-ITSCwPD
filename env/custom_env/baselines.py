45fd87b60078eda3c4fff809784b98a4ceaa68b7
romainducrocq <romain.g.ducrocq@gmail.com>
2021-07-26 17:18:36 +0200

    uniform baseline

--- env/custom_env/baselines.py
+++ env/custom_env/baselines.py
@@ -32,11 +32,52 @@ class UniformBaseline(BaselineMeta):
     def __init__(self, *args, **kwargs):
         super(UniformBaseline, self).__init__(*args, **kwargs)
 
+        self.tg = 10
+        self.ty = 3
+        self.tr = 2
+
+        self.scheduler, self.next_tl_id = None, None
+
     def reset(self):
         self.simulation_reset()
 
+        self.scheduler = TlScheduler(self.tg + self.ty + self.tr, self.tl_ids)
+        self.next_tl_id = self.scheduler.pop()[0]
+
+        for _ in range(self.tg):
+            self.simulation_step()
+
     def step(self, action):
-        self.simulation_step()
+        tl_id = self.next_tl_id
+
+        for evt in [
+            (self.ty, (tl_id, (self.get_next_red_phase_id(tl_id), self.tr))),
+            (self.ty + self.tr,
+             (tl_id, (self.get_new_green_phase_id(tl_id, self.get_next_green_phase_ryg_state(tl_id)), self.tg))),
+            (self.ty + self.tr + self.tg, (tl_id, None))
+        ]:
+            self.scheduler.push(*evt)
+
+        self.set_phase(tl_id, self.get_next_yellow_phase_id(tl_id))
+        self.set_phase_duration(tl_id, self.ty)
+
+        while True:
+            tl_evt = self.scheduler.pop()
+
+            if tl_evt is None:
+                self.simulation_step()
+
+            else:
+                tl_id, new_p = tl_evt
+
+                if new_p is not None:
+                    p, t = new_p
+                    self.set_phase(tl_id, p)
+                    self.set_phase_duration(tl_id, t)
+
+                else:
+                    self.next_tl_id = tl_id
+                    return
 
     def info(self):
         return {}
