dd8f80d63049ea392a5a82fbaa10361d2d10fa26
romainducrocq <romain.g.ducrocq@gmail.com>
2021-07-27 17:52:50 +0200

    log script

--- bin/observe.sh
+++ bin/observe.sh
@@ -2,7 +2,7 @@
 
 function run () {
 
-python3 observe.py -d save/1tls_3x3/DuelingDoubleDQNAgent_lr0.0001_model.pack -max_e 1000 -log y -log_s 1 -log_dir ./logs/test/1tls_3x3/con/
+python3 observe.py -d save/1tls_3x3/DuelingDoubleDQNAgent_lr0.0001_model.pack -max_e 10 -log y -log_s 1 -log_dir ./logs/test/1tls_3x3/con/
 
 }
 
